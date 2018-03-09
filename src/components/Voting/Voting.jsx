a91719bccf5f48cad809aad6cf76f5b29c63d1d8
astankovic <aleksa.stankovic@berkeley.edu>
2018-03-09 12:44:51 +0100

    Add voting bg text

--- src/components/Voting/Voting.jsx
+++ src/components/Voting/Voting.jsx
@@ -28,53 +28,57 @@ class Voting extends Component {
   render() {
     return (
       <div className="voting-wrapper">
-        <div className="meta">
-          <h2>Voting</h2>
-          <div className="nav">
-            <a
-              className={`${this.state.shown === 'current' ? 'active' : ''}`}
-              onClick={() => this.switch('current')}
-            >
-              Current Votes
-            </a>
-            <a
-              className={`${this.state.shown === 'past' ? 'active' : ''}`}
-              onClick={() => this.switch('past')}
-            >
-              Past Votes
-            </a>
-            <a
-              className={`${this.state.shown === 'submit' ? 'active' : ''}`}
-              onClick={() => this.switch('submit')}
-            >
-              Submit Proposal
-            </a>
-          </div>
-        </div>
-        <div className="content">
-          {
-            this.state.shown === 'current' &&
-            <Current />
-          }
-          {
-            this.state.shown === 'past' &&
-            <Past />
-          }
-          {
-            this.state.shown === 'submit' &&
-            <div className="submit-wrapper">
-              <h3>
-                Placeholder text title
-              </h3>
-              <p>
-                Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
-                literature from 45 BC, making it over 2000 years old. Lorem Ipsum is not simply
-                random text. It has roots in a piece of classical Latin literature from 45 BC,
-                making it over 2000 years old.
-              </p>
-              <Link to="/submit">Go to<br />submit page</Link>
+        <div className="bg-text voting-bg-text">voting</div>
+
+        <div className="voting-content-wrapper">
+          <div className="meta">
+            <h2>Voting</h2>
+            <div className="nav">
+              <a
+                className={`${this.state.shown === 'current' ? 'active' : ''}`}
+                onClick={() => this.switch('current')}
+              >
+                Current Votes
+              </a>
+              <a
+                className={`${this.state.shown === 'past' ? 'active' : ''}`}
+                onClick={() => this.switch('past')}
+              >
+                Past Votes
+              </a>
+              <a
+                className={`${this.state.shown === 'submit' ? 'active' : ''}`}
+                onClick={() => this.switch('submit')}
+              >
+                Submit Proposal
+              </a>
             </div>
-          }
+          </div>
+          <div className="content">
+            {
+              this.state.shown === 'current' &&
+              <Current />
+            }
+            {
+              this.state.shown === 'past' &&
+              <Past />
+            }
+            {
+              this.state.shown === 'submit' &&
+              <div className="submit-wrapper">
+                <h3>
+                  Placeholder text title
+                </h3>
+                <p>
+                  Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+                  literature from 45 BC, making it over 2000 years old. Lorem Ipsum is not simply
+                  random text. It has roots in a piece of classical Latin literature from 45 BC,
+                  making it over 2000 years old.
+                </p>
+                <Link to="/submit">Go to<br />submit page</Link>
+              </div>
+            }
+          </div>
         </div>
       </div>
     );
