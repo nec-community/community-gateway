81a271566f96d3129e04b7b5f3ca75b0eddd7db5
plutoegg <willharborne@Wills-MBP.connect>
2018-05-08 00:52:10 +0100

    style

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
 import App from './components/App/App';
 import store from './store';
-import ReactGA from 'react-ga';
+
 ReactGA.initialize('UA-117611254-2');
 ReactGA.pageview(window.location.href.split('#')[1]);
 
