a198619b5a476c528ebe077593da26ec0245697e
Nikola Klipa <klipa.nikola@gmail.com>
2018-08-30 13:58:31 +0200

    Added script for getting config, added new networks to deploy migration'

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -6,11 +6,16 @@ const DestructibleMiniMeFactory = artifacts.require("./DestructibleMiniMeTokenFa
 module.exports = function(deployer) {
 	
 	deployer.then(async () => {
+		let amount = 100000000;
+
 		await deployer.deploy(DestructibleMiniMeFactory);
 		let factory = await DestructibleMiniMeFactory.deployed();
 		
 		await deployer.deploy(NectarToken, factory.address, '0x0', 0, "Nectar", 18, "NEC", true);
 		let token = await NectarToken.deployed();
+
+		await token.generateTokens('0x00158a74921620b39e5c3afe4dca79feb2c2c143', 2 * amount);
+        await token.generateTokens('0xc26bf0fa0413d9a81470353589a50d4fb3f92a30', amount);
 
 		await deployer.deploy(TLM, factory.address, token.address);
 
--- solidity/truffle.js
+++ solidity/truffle.js
@@ -1,6 +1,8 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
+const dotenv           = require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
 
-var mnemonic = "";
+
+const mnemonic = process.env.ETHEREUM_ACCOUNT_MNEMONIC;
 
 module.exports = {
     solc: {
@@ -23,7 +25,23 @@ module.exports = {
                 return new HDWalletProvider(mnemonic, "https://testnet.decenter.com")
             },
             gas: 4698712
+        },
+        kovan: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, `https://kovan.decenter.com/`);
+            },
+            network_id: 42,
+            gasPrice: 2000000000, // 2 GWei
+            gas: 5400000,
+        },
+        rinkeby: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, `https://rinkeby.decenter.com/`);
+            },
+            network_id: 4,
+            gasPrice: 2000000000, // 2 GWei
+            gas: 5400000,
         }  
-    }
+    },
 };
 
