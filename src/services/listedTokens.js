cd219f2b8726df4c063517ab62c870a52b192d49
sterlu <sterlu.sd@gmail.com>
2019-04-29 17:05:28 +0200

    Mark listed tokens on pool page

---
+++ src/services/listedTokens.js
@@ -0,0 +1,5 @@
+export default [
+  '0x6810e776880c02933d47db1b9fc05908e5386b96', // Gnosis
+  '0x24dcc881e7dd730546834452f21872d5cb4b5293', // Scroll
+  '0x543ff227f64aa17ea132bf9886cab5db55dcaddf', // DAOstack
+];
