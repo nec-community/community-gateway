9e18fe54740d901efd9680e37ec18ab41f594af1
rnaverick <46535031+rnaverick@users.noreply.github.com>
2019-05-22 10:54:37 +0100

    Update index.js

--- proposed_tokens/index.js
+++ proposed_tokens/index.js
@@ -24,6 +24,7 @@ import FET from './fetch.json';
 import INK from './ink.json';
 import PMA from './pumapay.json';
 import TRIO from './tripio.json';
+import SOLVE from './solve.json';
 
 export default [
   ABYSS,
@@ -51,7 +52,8 @@ export default [
   FET,
   TRIO,
   INK,
-  PMA
+  PMA,
+  SOLVE,
 ].reduce((tmp, token) => {
   tmp[token.address.toLowerCase()] = token;
   return tmp;
