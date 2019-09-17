852f5ac221db8b0ffb6ed2a5af2c5857a3d746f6
rnaverick <46535031+rnaverick@users.noreply.github.com>
2019-09-17 14:19:32 +0100

    Update DaoGovernance.jsx (#2)

--- src/components/Landing/sections/DaoGovernance/DaoGovernance.jsx
+++ src/components/Landing/sections/DaoGovernance/DaoGovernance.jsx
@@ -7,16 +7,12 @@ export default function DaoGovernance() {
   return (
     <section className="landing__section">
       <p className="landing__section-content">
-        The future of Nectar will be completely in the hands of its holders. This will be enabled by
-        the launch of the necDAO. Participation in the necDAO will be available to all Nectar
-        holders and will have control over Nectar and the funds accumulated so far for NEC holders.
+        Would you like to determine the future of Nectar? The future of Nectar will be completely in the hands of its holders.
         <br />
         <br />
-        Ethfinex is committed to give the fee discounts stated to Nectar token holders, and to buy
-        and burn back tokens with revenues above the specified limits. Ethfinex will continue to
-        build, develop, and improve Ethfinex Trustless, and add new features to it. However, all
-        future decisions and any new utilities proposed for Nectar must be decided and added by its
-        DAO.
+        Enabled by the launch of the necDAO, Nectar holders will have control over Nectar and the funds accumulated in it so far. The necDAO will launch in December 2019 with accumulated funds of 17,000 ETH. Participation will be available to all Nectar holders. 
+      <br/>
+      DeversiFi is committed to providing specified fee discounts to Nectar token holders and to buying and burning tokens as specified above. DeversiFi will continue to build, develop and improve the DeversiFi portal, including introducing new features. However, all future decisions and new utilities proposed for Nectar must be decided and implemented by its DAO.
       </p>
       <Link to="/" className="landing__link dao-governance__link">
         Find out more
