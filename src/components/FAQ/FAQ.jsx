bef603c4849dadb2ebed5034cf407964265dbc26
NelleNaut <karl_dasilva@yahoo.co.uk>
2019-11-20 09:51:25 +0000

    Correct FAQ typos
    Correcting typos and clarifying information.

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -22,23 +22,23 @@ class FAQ extends Component {
               <a href="https://deversifi.com" target="_blank">
                 DeversiFi{' '}
               </a>{' '}
-             is a high-speed, comprehensive decentralised trading platform for digital assets and is the only existing exchange where traders can execute large orders at low cost directly from their privately owned wallets. With no sign-up or registration needed, DeversiFi supports all of the most popular wallet choices such as Metamask, Squarelink, Ledger, Trezor. Incubated over a 2 year period by Ethfinex under the trustless.ethfinex banner, DeversiFi is now a fully independent exchange that meets the needs of professional traders, without compromising on speed, security or choice and will be the first-to-market exchange to be powered by StarkWare technology, bringing instant settlement, deep liquidity, enhanced security and competitive fees to non-custodial trading.
+             is a high-speed, comprehensive decentralised trading platform for digital assets and is the only existing exchange where traders can execute large orders at low cost directly from their privately owned wallets. With no sign-up or registration needed, DeversiFi supports all of the most popular wallet choices such as Metamask, Squarelink, Portis, Ledger and Trezor. Incubated over a 2 year period by Ethfinex under the trustless.ethfinex banner, DeversiFi is now a fully independent exchange that meets the needs of professional traders, without compromising on speed, security or choice and will be the first-to-market exchange to be powered by StarkWare technology, bringing instant settlement, deep liquidity, enhanced security and competitive fees to non-custodial trading.
             </p>
           </div>
           <div className="faq-container">
           <h2>What is Nectar.Community?</h2>
             <p className="info-tip">
-              Nectar.Community is the decentralized governance portal for DeversiFi and is fully 
+              Nectar.Community is the decentralized governance portal for DeversiFi and is fully{' '}
               <a href="https://github.com/ethfinex/community-gateway" target="_blank">
                 open source.{' '}
               </a>{' '}
-               As well as functionality for submitting proposals and voting, the site also provides statistics, learning resources, and tools such as the trader leaderboard to allow Nectar token holders to be a part of the necDAO ecosystem as well as being involved in the NEC burn event auctions and promote the use of DeversiFi and a comprehensive and feature rich platform.
+               As well as functionality for submitting proposals and voting, the site also provides statistics, learning resources, and tools such as the trader leaderboard and the NEC burn event, which places Nectar token holders at the heart of the necDAO ecosystem.
           </p>
           </div>
           <div className="faq-container">
             <h2>What is the Nectar token (NEC)?</h2>
             <p className="info-tip">
-              The Nectar token is a deflationary governance and utility token created originally by Ethfinex as part of its journey towards decentralisation, and now underpinning DeversiFi’s continuation of that journey. The function of the token is to help grow the world’s largest exchange member base. Today the token, incentivises holders with fee discounts on DeversiFi, and empowers them, as necDAO members, to participate in the collective decision making and direction of Nectar and DeversiFi. Furthermore, every week, up to 50% of the revenues from DeversiFi trading fees will be used in an auction type NEC “buy and burn” event, thus reducing the supply over time. To find out more information about the Nectar token please read the {' '} 
+              The Nectar token is a deflationary governance and utility token created originally by Ethfinex as part of its journey towards decentralisation, and now underpinning DeversiFi’s continuation of that journey. The function of the token is to help grow the world’s largest decentralised exchange member base. Today the token incentivises holders with fee discounts on DeversiFi, and empowers them, as necDAO members, to participate in the collective decision making and direction of Nectar and DeversiFi. Furthermore, every week, up to 50% of the revenues from DeversiFi trading fees will be used in an auction type NEC “buy and burn” event, thus reducing the supply over time. To find out more information about the Nectar token please read the {' '} 
               <a href="https://nectar.community/whitepaper" target="_blank">
                 Whitepaper </a>{' '} and visit our {' '}
               <a href="https://support.deversifi.com/en/knowledgebase/5-the-nectar-token" target="_blank">
@@ -58,7 +58,7 @@ class FAQ extends Component {
                for trading against USDt and ETH, and {' '}
               <a href="https://www.bitfinex.com/" target="_blank">
                 Bitfinex </a>{' '}
-              against USD, ETH and BTC. This list will continue to grow as the implementation and usage of Nectar increases, and following the removal of a KYC whitelist, which now opens the token up to the wider DeFi ecosystem.
+              against USD, ETH and BTC. This list will continue to grow as the implementation and usage of Nectar increases, and follows the removal of a KYC whitelist, which now opens the token up to the wider DeFi ecosystem.
             </p>
           </div>
         </div>
@@ -99,7 +99,7 @@ It became clear however, that these voting processes were in need of a major ove
           <div className="faq-container">
             <h2>What is the DeversiFi Voting Token (DVT)?</h2>
             <p className="info-tip">
-              The DeversiFi Voting Token (previously the Ethfinex Voting Token EVT) is a distributed token used for the voting of ERC20 tokens to be listed on DeversiFi. Currently the voting process is on pause as we have been focusing on the Ethfinex to DeversiFi rebrand process.These tokens were distributed to Nectar Token (NEC) holders every time a vote starts in a 1:1 ratio, eg. a user holding 5,000 NEC on the date and time that the voting begins, received 5,000 DVT. These voting tokens were also tradeable on a USDt paired market on Ethfinex. As DVT tokens expire and disappear at the end of every voting period, they are not meant for long term hodling, as such practice will result in getting rekt.
+              The DeversiFi Voting Token (previously the Ethfinex Voting Token EVT) is a distributed token used for the voting of ERC20 tokens to be listed on DeversiFi. Currently the voting process is on pause as we have been focusing on the Ethfinex to DeversiFi rebrand process. These tokens were distributed to Nectar Token (NEC) holders every time a vote started in a 1:1 ratio, eg. a user holding 5,000 NEC on the date and time that the voting began, received 5,000 DVT. These voting tokens were also tradeable on a USDt paired market on Ethfinex. As DVT tokens expire and disappear at the end of every voting period, they are not meant for long term hodling, as such practice will result in getting rekt.
             </p>
           </div>
           <div className="faq-container">
