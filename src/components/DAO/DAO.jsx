5ba070e567b211089f1b0d32e26273a43e425543
Will Harborne <will.harborne@googlemail.com>
2019-12-18 13:05:20 +0000

    Open participation to earn REP

--- src/components/DAO/DAO.jsx
+++ src/components/DAO/DAO.jsx
@@ -82,13 +82,6 @@ class DAO extends Component {
               <a
                 className="side-badge"
                 href="#"
-              >
-                Earn voting power here once the first stage of the DAO launches
-              </a>
-              {/*
-              <a
-                className="side-badge"
-                href="#"
                 onClick={this.toggleTermsModal}
               >
               Participate Now
@@ -97,7 +90,6 @@ class DAO extends Component {
                 isOpen={this.state.isTermsModalOpen}
                 handleClose={this.toggleTermsModal}
               />
-              */}
             </div>
           </div>
           <div className="toc-wrapper">
