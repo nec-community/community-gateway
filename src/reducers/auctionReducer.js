ab5228b5474656bed4c7dbe6f9237c3c106829b1
AliMeer <ali@purple7.com>
2020-01-10 18:59:04 +0000

    added new actions and reducers

--- src/reducers/auctionReducer.js
+++ src/reducers/auctionReducer.js
@@ -2,14 +2,16 @@ import {
   FETCH_CIRCULATING_NEC_DATA,
   FETCH_BURNED_NEC,
   FETCH_DEVERSIFI_NEC_ETH_DATA,
-  FETCH_NEXT_AUCTION_ETH_DATA
+  FETCH_NEXT_AUCTION_ETH_DATA,
+  FETCH_CURRENT_AUCTION_SUMMARY,
+  FETCH_AUCTION_INTERVAL_DATA,
 } from '../actions/actionTypes';
 
 const INITIAL_STATE = {
   circulatingNecData: [],
   burnedNecData: [],
   deversifiNecEthPriceData: [],
-  nextAuctionEthData: []
+  nextAuctionEthData: [],
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -17,23 +19,33 @@ export default (state = INITIAL_STATE, action) => {
     case FETCH_CIRCULATING_NEC_DATA:
       return {
         ...state,
-        circulatingNecData: action.circulatingNecData
-      }
+        circulatingNecData: action.circulatingNecData,
+      };
     case FETCH_BURNED_NEC:
       return {
         ...state,
-        burnedNecData: action.burnedNecData
-      }
+        burnedNecData: action.burnedNecData,
+      };
     case FETCH_DEVERSIFI_NEC_ETH_DATA:
       return {
         ...state,
-        deversifiNecEthData: action.deversifiNecEthData
-      }
+        deversifiNecEthData: action.deversifiNecEthData,
+      };
     case FETCH_NEXT_AUCTION_ETH_DATA:
       return {
         ...state,
-        nextAuctionEthData: action.nextAuctionEthData
-      }
+        nextAuctionEthData: action.nextAuctionEthData,
+      };
+    case FETCH_CURRENT_AUCTION_SUMMARY:
+      return {
+        ...state,
+        currentAuctionSummary: action.currentAuctionSummary,
+      };
+    case FETCH_AUCTION_INTERVAL_DATA:
+      return {
+        ...state,
+        auctionIntervalData: action.auctionIntervalData,
+      };
     default:
       return state;
   }
